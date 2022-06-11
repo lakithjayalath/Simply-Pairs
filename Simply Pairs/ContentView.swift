f418837480c92d9058917646d7f60e51c61fd5bd
lakithjayalath <lakith54321@gmail.com>
2022-06-12 01:50:51 +0530

    Initial Commit

---
+++ Simply Pairs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simply Pairs
+//
+//  Created by Lakith Jayalath on 2022-06-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simply Pairs/Simply_PairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simply_PairsApp.swift
+//  Simply Pairs
+//
+//  Created by Lakith Jayalath on 2022-06-12.
+//
+
+import SwiftUI
+
+@main
+struct Simply_PairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
